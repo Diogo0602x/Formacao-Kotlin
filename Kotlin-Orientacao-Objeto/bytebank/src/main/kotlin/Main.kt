3ba268c5548710225951ee868e9d29c9dc0faaba
Diogo Sales Teixeira <diogoteixeira0602@gmail.com>
2022-10-13 11:46:32 -0300

    Aula 1 terminada

--- Kotlin-Orientacao-Objeto/bytebank/src/main/kotlin/Main.kt
+++ Kotlin-Orientacao-Objeto/bytebank/src/main/kotlin/Main.kt
@@ -1,7 +1,12 @@
 fun main(args: Array<String>) {
-    println("Projeto iniciado!")
+    println("Bem vindo ao Bytebank")
+    val titular = "Diogo"
+    val numeroConta = 1000
+    var saldo = 0.0
+    saldo = 100 + 2.0
+    saldo += 200
 
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
+    println("titular $titular")
+    println("número da conta $numeroConta")
+    println("saldo da conta $saldo")
 }
