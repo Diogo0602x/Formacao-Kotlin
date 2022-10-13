bfa04f9a5d5fdf315c35aadc0e03d86c9989c05f
Diogo Sales Teixeira <diogoteixeira0602@gmail.com>
2022-10-13 11:40:39 -0300

    Initial commit

---
+++ Kotlin-Orientacao-Objeto/bytebank/src/main/kotlin/Main.kt
@@ -0,0 +1,7 @@
+fun main(args: Array<String>) {
+    println("Projeto iniciado!")
+
+    // Try adding program arguments via Run/Debug configuration.
+    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
+    println("Program arguments: ${args.joinToString()}")
+}
