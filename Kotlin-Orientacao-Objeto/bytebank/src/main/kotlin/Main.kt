10489dc2edecd48c5481a9a75c2b5c43ca86fa5b
Diogo Sales Teixeira <diogoteixeira0602@gmail.com>
2022-10-14 18:38:13 -0300

    Curso finalizado

--- Kotlin-Orientacao-Objeto/bytebank/src/main/kotlin/Main.kt
+++ Kotlin-Orientacao-Objeto/bytebank/src/main/kotlin/Main.kt
@@ -1,74 +1,72 @@
 fun main() {
     println("Bem vindo ao Bytebank")
 
-    val contaDiogo = Conta()
-    contaDiogo.titular = "Diogo"
-    contaDiogo.numero = 1000
-    contaDiogo.setSaldo(200.0)
+    val contaDiogo = Conta(titular = "Diogo", numero = 1000)
+    contaAlex.deposita(200.0)
 
-    val contaFran = Conta()
-    contaFran.titular = "Fran"
-    contaFran.numero = 1001
-    contaFran.setSaldo(300.0)
+    val contaFran = Conta(numero = 1001, titular = "Fran")
+    contaFran.deposita(300.0)
 
     println(contaFran.titular)
     println(contaFran.numero)
-    println(contaFran.getSaldo())
+    println(contaFran.saldo)
 
     println(contaDiogo.titular)
     println(contaDiogo.numero)
-    println(contaDiogo.getSaldo())
+    println(contaDiogo.saldo)
 
-//    println("Depositando na conta do Diogo")
-//    contaDiogo.deposita(50.0)
-//    println(contaDiogo.saldo)
-//
-//    println("Depositando na conta da Fran")
-//    contaFran.deposita(70.0)
-//    println(contaFran.saldo)
-//
-//    println("Sacando na conta do Diogo")
-//    contaDiogo.saca(250.0)
-//    println(contaDiogo.saldo)
-//
-//    println("Sacando na conta da Fran")
-//    contaFran.saca(100.0)
-//    println(contaFran.saldo)
-//
-//    println("Saque em excesso na conta do Diogo")
-//    contaDiogo.saca(100.0)
-//    println(contaDiogo.saldo)
-//
-//    println("Saque em excesso na conta da Fran")
-//    contaFran.saca(500.0)
-//    println(contaFran.saldo)
-//
-//    println("Transferência da conta da Fran para o Diogo")
-//
-//    if(contaFran.transfere(300.0, contaDiogo)){
-//        println("Transferência sucedida")
-//    } else {
-//        println("Falha na transferência")
-//    }
-//
-//    println(contaDiogo.saldo)
-//    println(contaFran.saldo)
+    println("Depositando na conta do Diogo")
+    contaDiogo.deposita(50.0)
+    println(contaDiogo.saldo)
+
+    println("Depositando na conta da Fran")
+    contaFran.deposita(70.0)
+    println(contaFran.saldo)
+
+    println("Sacando na conta do Diogo")
+    contaDiogo.saca(250.0)
+    println(contaDiogo.saldo)
+
+    println("Sacando na conta da Fran")
+    contaFran.saca(100.0)
+    println(contaFran.saldo)
+
+    println("Saque em excesso na conta do Diogo")
+    contaDiogo.saca(100.0)
+    println(contaDiogo.saldo)
+
+    println("Saque em excesso na conta da Fran")
+    contaFran.saca(500.0)
+    println(contaFran.saldo)
+
+    println("Transferência da conta da Fran para o Diogo")
+
+    if(contaFran.transfere(300.0, contaDiogo)){
+        println("Transferência sucedida")
+    } else {
+        println("Falha na transferência")
+    }
+
+    println(contaDiogo.saldo)
+    println(contaFran.saldo)
 }
 
-class Conta {
-    var titular = ""
-    var numero = 0
-    private var saldo = 0.0
+class Conta(
+    var titular: String,
+    val numero: Int
+) {
+    var saldo = 0.0
+        private set
 
     fun deposita(valor: Double) {
-        this.saldo += valor
+        if (valor > 0) {
+            this.saldo += valor
+        }
     }
 
     fun saca(valor: Double) {
         if (saldo >= valor) {
             saldo -= valor
-        } else {
-            println("Valor maior do que disponível na conta")
         }
     }
 
@@ -79,16 +77,6 @@ class Conta {
             return true
         }
         return false
-    }
-
-    fun getSaldo(): Double {
-        return saldo
-    }
-
-    fun setSaldo(valor: Double) {
-        if(valor > 0) {
-            saldo = valor
-        }
     }
 }
 
