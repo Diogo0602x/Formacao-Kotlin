300f057980af662e075c67c2875bdd85da4d640f
Diogo Sales Teixeira <diogoteixeira0602@gmail.com>
2022-10-14 16:20:26 -0300

    Aula 3 terminada

--- Kotlin-Orientacao-Objeto/bytebank/src/main/kotlin/Main.kt
+++ Kotlin-Orientacao-Objeto/bytebank/src/main/kotlin/Main.kt
@@ -1,5 +1,65 @@
 fun main() {
     println("Bem vindo ao Bytebank")
+
+    val contaDiogo = Conta()
+    contaDiogo.titular = "Diogo"
+    contaDiogo.numero = 1000
+    contaDiogo.saldo = 200.0
+
+    val contaFran = Conta()
+    contaFran.titular = "Fran"
+    contaFran.numero = 1001
+    contaFran.saldo = 300.0
+
+    println(contaFran.titular)
+    println(contaFran.numero)
+    println(contaFran.saldo)
+
+    println(contaDiogo.titular)
+    println(contaDiogo.numero)
+    println(contaDiogo.saldo)
+
+    println("Depositando na conta do Diogo")
+    deposita(contaDiogo, 50.0)
+    println(contaDiogo.saldo)
+
+    println("Depositando na conta da Fran")
+    deposita(contaFran, 70.0)
+    println(contaFran.saldo)
+}
+
+fun deposita(conta: Conta, valor: Double) {
+    conta.saldo += valor
+}
+
+class Conta {
+    var titular = ""
+    var numero = 0
+    var saldo = 0.0
+}
+
+fun testaCopiasEReferencias() {
+    val numeroX = 10
+    var numeroY = numeroX
+    numeroY++
+
+    println("numeroX $numeroX")
+    println("numeroY $numeroY")
+
+    val contaJoao = Conta()
+    contaJoao.titular = "Joao"
+    var contaMaria = Conta()
+    contaMaria.titular = "Maria"
+    contaJoao.titular = "Diogo"
+
+    println("titular conta joao: ${contaJoao.titular}")
+    println("titular conta maria: ${contaMaria.titular}")
+
+    println(contaJoao)
+    println(contaMaria)
+}
+
+fun testaLacos() {
     var i = 0
     while(i < 5) {
         val titular: String = "Diogo $i"
@@ -13,18 +73,16 @@ fun main() {
         i++
     }
 
-//    for (i in 5 downTo 1 ) {
-//        val titular: String = "Diogo $i"
-//        val numeroConta: Int = 1000 + i
-//        var saldo = i + 10.0
-//
-//        println("titular $titular")
-//        println("número da conta $numeroConta")
-//        println("saldo da conta $saldo")
-//        println()
-//    }
+    for (i in 5 downTo 1 ) {
+        val titular: String = "Diogo $i"
+        val numeroConta: Int = 1000 + i
+        var saldo = i + 10.0
 
-//    testaCondicoes(saldo)
+        println("titular $titular")
+        println("número da conta $numeroConta")
+        println("saldo da conta $saldo")
+        println()
+    }
 }
 
 fun testaCondicoes(saldo: Double) {
