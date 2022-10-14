3e89982c48630f34af732e674979bcde22f17c3d
Diogo Sales Teixeira <diogoteixeira0602@gmail.com>
2022-10-14 11:34:15 -0300

    Aula 2 terminada

--- Kotlin-Orientacao-Objeto/bytebank/src/main/kotlin/Main.kt
+++ Kotlin-Orientacao-Objeto/bytebank/src/main/kotlin/Main.kt
@@ -1,12 +1,44 @@
-fun main(args: Array<String>) {
+fun main() {
     println("Bem vindo ao Bytebank")
-    val titular = "Diogo"
-    val numeroConta = 1000
-    var saldo = 0.0
-    saldo = 100 + 2.0
-    saldo += 200
+    var i = 0
+    while(i < 5) {
+        val titular: String = "Diogo $i"
+        val numeroConta: Int = 1000 + i
+        var saldo = i + 10.0
 
-    println("titular $titular")
-    println("número da conta $numeroConta")
-    println("saldo da conta $saldo")
+        println("titular $titular")
+        println("número da conta $numeroConta")
+        println("saldo da conta $saldo")
+        println()
+        i++
+    }
+
+//    for (i in 5 downTo 1 ) {
+//        val titular: String = "Diogo $i"
+//        val numeroConta: Int = 1000 + i
+//        var saldo = i + 10.0
+//
+//        println("titular $titular")
+//        println("número da conta $numeroConta")
+//        println("saldo da conta $saldo")
+//        println()
+//    }
+
+//    testaCondicoes(saldo)
+}
+
+fun testaCondicoes(saldo: Double) {
+    if (saldo > 0.0) {
+        println("Conta com saldo positivo!")
+    } else if (saldo == 0.0) {
+        println("Conta com saldo neutro")
+    } else {
+        println("Conta com saldo negativo")
+    }
+
+    when {
+        saldo > 0.0 -> println("Conta com saldo positivo!")
+        saldo == 0.0 -> println("Conta com saldo neutro")
+        else -> println("Conta com saldo negativo")
+    }
 }
